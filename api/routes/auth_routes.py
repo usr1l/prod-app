c01481b1f8ecd77966b553752edce01d16360323
tkny <zhtony0398@gmail.com>
2024-02-11 16:28:08 -0500

    route blueprints

--- api/routes/auth_routes.py
+++ api/routes/auth_routes.py
@@ -4,6 +4,6 @@ from flask import Blueprint, request, jsonify
 auth_routes = Blueprint("auth_routes", __name__)
 
 
-@auth_routes.route("/api/healthchecker", methods=["GET"])
+@auth_routes.route("/healthchecker", methods=["GET"])
 def healthchecker():
     return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
