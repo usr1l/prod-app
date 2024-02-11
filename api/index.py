8b6aafbe5d9e7104e677bd6d31e2ef9355021bef
tkny <zhtony0398@gmail.com>
2024-02-11 17:51:39 -0500

    dotenv

--- api/index.py
+++ api/index.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from api.config import Config
 from api.models import db
 from api.routes import auth_routes
-from api.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,17 +21,17 @@ CORS(app, supports_credentials=True)
 # @app.route("/api/healthchecker", methods=["GET"])
 # def healthchecker():
 #     return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
-@app.after_request
-def inject_csrf_token(response):
-    response.set_cookie(
-        'csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
-        samesite='Strict' if os.environ.get(
-            'FLASK_ENV') == 'production' else None,
-        httponly=True
-    )
-    return response
+# @app.after_request
+# def inject_csrf_token(response):
+#     response.set_cookie(
+#         'csrf_token',
+#         generate_csrf(),
+#         secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
+#         samesite='Strict' if os.environ.get(
+#             'FLASK_ENV') == 'production' else None,
+#         httponly=True
+#     )
+#     return response
 
 
 if __name__ == "__main__":
