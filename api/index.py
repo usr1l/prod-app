2cc10488b126e3ee9c9dc54478670ca1c60cf062
tkny <zhtony0398@gmail.com>
2024-02-11 23:29:50 -0500

    comments

--- api/index.py
+++ api/index.py
@@ -23,17 +23,17 @@ if os.environ.get('FLASK_ENV') == 'production':
         db.create_all()
 
 
-@app.after_request
-def inject_csrf_token(response):
-    response.set_cookie(
-        'csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
-        samesite='Strict' if os.environ.get(
-            'FLASK_ENV') == 'production' else None,
-        httponly=True
-    )
-    return response
+# @app.after_request
+# def inject_csrf_token(response):
+#     response.set_cookie(
+#         'csrf_token',
+#         generate_csrf(),
+#         secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
+#         samesite='Strict' if os.environ.get(
+#             'FLASK_ENV') == 'production' else None,
+#         httponly=True
+#     )
+#     return response
 
 
 if __name__ == "__main__":
