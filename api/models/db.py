29999409ed757aeac00bcc8725d845ca8bbd0248
tkny <zhtony0398@gmail.com>
2024-02-12 18:55:36 -0500

    schema removal

--- api/models/db.py
+++ api/models/db.py
@@ -9,8 +9,8 @@ db = SQLAlchemy()
 # # helper function for adding prefix to foreign key column references in production
 
 
-def add_prefix_for_prod(attr):
-    if environment == "production":
-        return f"{SCHEMA}.{attr}"
-    else:
-        return attr
+# def add_prefix_for_prod(attr):
+#     if environment == "production":
+#         return f"{SCHEMA}.{attr}"
+#     else:
+#         return attr
