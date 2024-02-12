fba3757f0c4db7dab296a04e4ec905a9bb32cc27
tkny <zhtony0398@gmail.com>
2024-02-12 02:53:00 -0500

    base

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from http.server import BaseHTTPRequestHandler
 from api.config import Config
 from api.models import db, User
 from api.routes import auth_routes
@@ -23,9 +24,10 @@ CORS(app, supports_credentials=True)
 #         db.create_all()
 
 
-@app.route('/api/', methods=['GET'])
-def hello_world():
-    return "Hello, World!"
+class handler(BaseHTTPRequestHandler):
+    @app.route('/api/', methods=['GET'])
+    def hello_world():
+        return "Hello, World!"
 
 
 @app.route("/api/healthchecker", methods=["GET"])
