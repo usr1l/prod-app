a3494da930bec2425b2d6311fce19a3e3c806f7f
tkny <zhtony0398@gmail.com>
2024-03-12 17:41:56 -0400

    latest

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,24 +3,34 @@ import OpenModalButton from './OpenModalButton';
 import '@app/globals.css';
 
 const Button = ({
-  children,
+  buttonText,
   type,
   onClick,
   disableButton,
   buttonClass,
-  value
+  value,
+  icon,
+  alt
 }) => {
 
   return (
     <div className='flex justify-center items-center'>
       <button
-        className={`px-2 py-20 outline-none border-0 cursor-pointer ${buttonClass}`}
+        className={`flex gap-1 justify-center px-4 py-2.5 text-base text-center text-white whitespace-nowrap rounded-3xl shadow-sm bg-zinc-900 ${buttonClass}`}
         onClick={onClick}
         type={type}
         disabled={disableButton || false}
         value={value}
       >
-        {children}
+        <img
+          loading="lazy"
+          src={icon}
+          className="shrink-0 my-auto aspect-[0.74] fill-white w-[11px]"
+          alt={alt}
+        />
+        <span className='grow'>
+          {buttonText}
+        </span>
       </button>
     </div>
   )
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,17 +1,16 @@
 "use client"
 
 import Link from "next/link";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { thunkAuthenticate } from "@lib/store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import ProfileButton from './ProfileButton';
 import SiteLogo from "@components/SiteLogo";
 import BottomNav from "./BottomNav";
+import SearchBar from "./SearchBar";
+import Button from "@components/Button";
 import '@app/globals.css';
 
 export default function Navigation() {
-
   const sessionUser = useSelector(state => state.session.user);
 
   return (
@@ -19,6 +18,8 @@ export default function Navigation() {
       className="flex w-auto items-center shadow-lg box-border justify-between sticky h-20 bg-white top-0 right-0 left-0 z-50 border-b border-gray-400 px-6"
     >
       <SiteLogo />
+      <SearchBar placeholder={'Search'} icon={'/search.png'} />
+      <Button buttonText={'Generate'} icon={'/sparkles.png'} />
       {sessionUser && (
         <ProfileButton user={sessionUser} />
       )}
