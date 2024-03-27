5f7c54417ec381420930d2815f14d06e85138388
tkny <zhtony0398@gmail.com>
2024-03-27 14:53:24 -0400

    textarea input

--- src/app/generate/page.jsx
+++ src/app/generate/page.jsx
@@ -1,30 +1,27 @@
 'use client';
 
 import AuthProtect from '@AuthProtect';
-import React from 'react';
-import '@app/globals.css';
+import React, { useState } from 'react';
 import Button from '@components/Button';
+import '@app/globals.css';
 
 function Generate() {
+  const [ textInput, setTextInput ] = useState('');
+  const [ fileInput, setFileInput ] = useState('');
+
   const navButtonClass = 'px-0 text-[16px]';
-  const navContainerClass = 'mr-4 underline px-0';
+  const navContainerClass = 'mr-4 py-2 box-border opacity-60 border-b-2 flex cursor-pointer ';
   return (
     <>
-      <div
-        className='w-auto m-4 border-b-2 border-gray-300 shadow-md min-w-[1200px]'
-      >
-        <h1
-          className='h-[100px] w-auto px-10 flex items-center font-bold shadow-lg'
-        >MAGIC GENERATOR
-          <div
-            className='rounded-3xl bg-site-purple w-28 flex justify-center items-center mx-6 bg-opacity-20 text-[13px] font-normal'
-          >Powered by AI</div>
+      <div className='w-auto m-4 border-b-2 border-gray-300 shadow-md min-w-[900px]'>
+        <h1 className='h-[100px] w-auto px-10 flex items-center font-bold shadow-lg'>
+          MAGIC GENERATOR
+          <div className='rounded-3xl bg-site-purple w-28 flex justify-center items-center mx-6 bg-opacity-20 text-[13px] font-normal'>
+            Powered by AI
+          </div>
         </h1>
-        <div
-          className='w-auto border-black px-16 py-8 flex box-border'
-        >
+        <div className='w-auto border-black px-16 py-8 flex box-border'>
           <div className='w-[60%]'>
-
             <h1 className='font-bold text-[26px]'>Customize study sets in an instant</h1>
             <h2 className='text-[20px]'>Upload notes, slides, readings or recordings</h2>
             <nav className='w-auto flex mt-4'>
@@ -37,11 +34,23 @@ function Generate() {
                 buttonText={'Upload File from PC'}
                 buttonClass={navButtonClass}
                 containerClass={navContainerClass}
+                disableButton={true}
               />
             </nav>
+            <div className='w-auto mt-4'>
+              <textarea
+                className="w-full h-40 bg-site-black bg-opacity-10 rounded-xl focus:outline-none p-4"
+                placeholder="Paste text here."
+                aria-label="Paste text here"
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}
+              ></textarea>
+            </div>
           </div>
-          <div className='w-[40%] flex flex-col'>
-
+          <div className='w-[40%] flex flex-col items-center relative'>
+            <div className='w-[70%] border-2 border-black'>
+              <h1 className=''>Powered by AI</h1>
+            </div>
           </div>
         </div>
       </div>
